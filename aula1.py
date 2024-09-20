d805d6e93a9b8ae932039f2c1ae38e20d3031f0b
ViniciusAraujo-Dev <vinicius.sm.araujo@gmail.com>
2024-09-20 18:00:45 -0300

    2° commit - criando a janela principal e suas configuracoes

---
+++ aula1.py
@@ -0,0 +1,20 @@
+from tkinter import *
+
+janela = Tk() #root
+
+class Application():                            # uma classe que mantem a janela aberta em loop
+    def __init__(self):
+        self.janela = janela                    # como janela nao esta na classe, deve-se fazer uma equivalencia
+        self.tela()                             # chama a funcao tela
+        janela.mainloop()
+    def tela(self):
+        self.janela.title("Cadastro de Clientes") #função para as configurações da tela
+        self.janela.configure(background='#1e3743')
+        self.janela.geometry("700x500")
+        self.janela.resizable(True, True)
+        self.janela.maxsize(width="900", height="700")
+        self.janela.minsize(width="400", height="300")
+
+
+
+Application()
