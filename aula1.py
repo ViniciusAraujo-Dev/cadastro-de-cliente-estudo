eff6465b38d5525de7bfc8f8bb24cd8b3ef66dbe
ViniciusAraujo-Dev <vinicius.sm.araujo@gmail.com>
2024-09-22 00:58:40 -0300

    5° commit - criacao das labels e entrys do frame 1, mudando o minseize da janela

--- aula1.py
+++ aula1.py
@@ -15,7 +15,7 @@ class Application():                            # uma classe que mantem a janela
         self.janela.geometry("700x500")
         self.janela.resizable(True, True)
         self.janela.maxsize(width="900", height="700")
-        self.janela.minsize(width="400", height="300")
+        self.janela.minsize(width="500", height="400")
     def frames_da_tela(self):
         self.frame_1 = Frame(self.janela, bd=4, bg='#dfe3ee', highlightbackground='#759fe6', highlightthickness=3)
         # para relx e rely, e a posicao relativa do objeto em relacao a tela, vai de 0 a 1, onde 0 e totalmente a esquerda e 1 a direita
@@ -25,21 +25,47 @@ class Application():                            # uma classe que mantem a janela
         self.frame_2 = Frame(self.janela, bd=4, bg='#dfe3ee', highlightbackground='#759fe6', highlightthickness=3)
         self.frame_2.place(relx= 0.02, rely=0.5, relwidth=0.95, relheight=0.46)
     def widtgets_frame1(self):
-        ### Criando o botão limpar
+        ### Criação do botão limpar
         self.bt_limpar = Button(self.frame_1, text="Limpar")
         self.bt_limpar.place(relx=0.2, rely=0.1, relwidth=0.1, relheight=0.15)
-        ### Criando o botão buscar
+        ### Criação do botão buscar
         self.bt_buscar = Button(self.frame_1, text="Buscar")
         self.bt_buscar.place(relx=0.3, rely=0.1, relwidth=0.1, relheight=0.15)
-        ### Criando o botão novo
+        ### Criação do botão novo
         self.bt_novo = Button(self.frame_1, text="Novo")
         self.bt_novo.place(relx=0.6, rely=0.1, relwidth=0.1, relheight=0.15)
-        ### Criando o botão alterar
+        ### Criação do botão alterar
         self.bt_Alterar = Button(self.frame_1, text="Alterar")
         self.bt_Alterar.place(relx=0.7, rely=0.1, relwidth=0.1, relheight=0.15)
-        ### Criando o botão apagar
+        ### Criação do botão apagar
         self.bt_Apagar = Button(self.frame_1, text="Apagar")
         self.bt_Apagar.place(relx=0.8, rely=0.1, relwidth=0.1, relheight=0.15)
-        
 
+        ### Criação da label e entrada do código
+        self.lb_codigo = Label(self.frame_1, text="Código")
+        self.lb_codigo.place(relx=0.05, rely=0.05)
+
+        self.codigo_entry = Entry(self.frame_1)
+        self.codigo_entry.place(relx=0.05, rely=0.15, relwidth=0.08)
+
+        ### Criação da label e entrada do nome
+        self.lb_nome = Label(self.frame_1, text="Nome")
+        self.lb_nome.place(relx=0.05, rely=0.35)
+
+        self.nome_entry = Entry(self.frame_1)
+        self.nome_entry.place(relx=0.05, rely=0.45, relwidth=0.8)
+
+         ### Criação da label e entrada do telefone
+        self.lb_telefone = Label(self.frame_1, text="Telefone")
+        self.lb_telefone.place(relx=0.05, rely=0.60)
+
+        self.telefone_entry = Entry(self.frame_1)
+        self.telefone_entry.place(relx=0.05, rely=0.7, relwidth=0.4)
+
+         ### Criação da label e entrada da cidade
+        self.lb_cidade = Label(self.frame_1, text="Cidade")
+        self.lb_cidade.place(relx=0.5, rely=0.6)
+
+        self.cidade_entry = Entry(self.frame_1)
+        self.cidade_entry.place(relx=0.5, rely=0.7, relwidth=0.4)
 Application()
