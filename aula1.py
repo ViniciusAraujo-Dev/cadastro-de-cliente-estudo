6d8039b56adefac1b555e750ce168e35223ff84a
ViniciusAraujo-Dev <vinicius.sm.araujo@gmail.com>
2024-09-22 00:35:32 -0300

    3° commit - criando os botoes do frame 1

--- aula1.py
+++ aula1.py
@@ -7,6 +7,7 @@ class Application():                            # uma classe que mantem a janela
         self.janela = janela                    # como janela nao esta na classe, deve-se fazer uma equivalencia
         self.tela()                             # chama a funcao tela
         self.frames_da_tela()
+        self.widtgets_frame1()
         janela.mainloop()
     def tela(self):
         self.janela.title("Cadastro de Clientes") #função para as configurações da tela
@@ -23,6 +24,22 @@ class Application():                            # uma classe que mantem a janela
 
         self.frame_2 = Frame(self.janela, bd=4, bg='#dfe3ee', highlightbackground='#759fe6', highlightthickness=3)
         self.frame_2.place(relx= 0.02, rely=0.5, relwidth=0.95, relheight=0.46)
-
+    def widtgets_frame1(self):
+        ### Criando o botão limpar
+        self.bt_limpar = Button(self.frame_1, text="Limpar")
+        self.bt_limpar.place(relx=0.2, rely=0.1, relwidth=0.1, relheight=0.15)
+        ### Criando o botão buscar
+        self.bt_buscar = Button(self.frame_1, text="Buscar")
+        self.bt_buscar.place(relx=0.3, rely=0.1, relwidth=0.1, relheight=0.15)
+        ### Criando o botão novo
+        self.bt_novo = Button(self.frame_1, text="Novo")
+        self.bt_novo.place(relx=0.6, rely=0.1, relwidth=0.1, relheight=0.15)
+        ### Criando o botão alterar
+        self.bt_Alterar = Button(self.frame_1, text="Alterar")
+        self.bt_Alterar.place(relx=0.7, rely=0.1, relwidth=0.1, relheight=0.15)
+        ### Criando o botão apagar
+        self.bt_Apagar = Button(self.frame_1, text="Apagar")
+        self.bt_Apagar.place(relx=0.8, rely=0.1, relwidth=0.1, relheight=0.15)
+        
 
 Application()
