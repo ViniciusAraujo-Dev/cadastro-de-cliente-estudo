8f1a9a93dbc094b0a01410ff27512b6c1104b05f
ViniciusAraujo-Dev <vinicius.sm.araujo@gmail.com>
2024-09-22 03:07:37 -0300

    6° commit - estilizacao do frame 1

--- aula1.py
+++ aula1.py
@@ -26,44 +26,44 @@ class Application():                            # uma classe que mantem a janela
         self.frame_2.place(relx= 0.02, rely=0.5, relwidth=0.95, relheight=0.46)
     def widtgets_frame1(self):
         ### Criação do botão limpar
-        self.bt_limpar = Button(self.frame_1, text="Limpar")
+        self.bt_limpar = Button(self.frame_1, text="Limpar", bd=2, bg='#107db2', fg='white')
         self.bt_limpar.place(relx=0.2, rely=0.1, relwidth=0.1, relheight=0.15)
         ### Criação do botão buscar
-        self.bt_buscar = Button(self.frame_1, text="Buscar")
+        self.bt_buscar = Button(self.frame_1, text="Buscar", bd=2, bg='#107db2', fg='white')
         self.bt_buscar.place(relx=0.3, rely=0.1, relwidth=0.1, relheight=0.15)
         ### Criação do botão novo
-        self.bt_novo = Button(self.frame_1, text="Novo")
+        self.bt_novo = Button(self.frame_1, text="Novo", bd=2, bg='#107db2', fg='white')
         self.bt_novo.place(relx=0.6, rely=0.1, relwidth=0.1, relheight=0.15)
         ### Criação do botão alterar
-        self.bt_Alterar = Button(self.frame_1, text="Alterar")
+        self.bt_Alterar = Button(self.frame_1, text="Alterar", bd=2, bg='#107db2', fg='white')
         self.bt_Alterar.place(relx=0.7, rely=0.1, relwidth=0.1, relheight=0.15)
         ### Criação do botão apagar
-        self.bt_Apagar = Button(self.frame_1, text="Apagar")
+        self.bt_Apagar = Button(self.frame_1, text="Apagar", bd=2, bg='#107db2', fg='white')
         self.bt_Apagar.place(relx=0.8, rely=0.1, relwidth=0.1, relheight=0.15)
 
         ### Criação da label e entrada do código
-        self.lb_codigo = Label(self.frame_1, text="Código")
+        self.lb_codigo = Label(self.frame_1, text="Código", bg='#dfe3ee', fg='#107db2')
         self.lb_codigo.place(relx=0.05, rely=0.05)
 
         self.codigo_entry = Entry(self.frame_1)
         self.codigo_entry.place(relx=0.05, rely=0.15, relwidth=0.08)
 
         ### Criação da label e entrada do nome
-        self.lb_nome = Label(self.frame_1, text="Nome")
+        self.lb_nome = Label(self.frame_1, text="Nome", bg='#dfe3ee', fg='#107db2')
         self.lb_nome.place(relx=0.05, rely=0.35)
 
         self.nome_entry = Entry(self.frame_1)
         self.nome_entry.place(relx=0.05, rely=0.45, relwidth=0.8)
 
          ### Criação da label e entrada do telefone
-        self.lb_telefone = Label(self.frame_1, text="Telefone")
+        self.lb_telefone = Label(self.frame_1, text="Telefone", bg='#dfe3ee', fg='#107db2')
         self.lb_telefone.place(relx=0.05, rely=0.60)
 
         self.telefone_entry = Entry(self.frame_1)
         self.telefone_entry.place(relx=0.05, rely=0.7, relwidth=0.4)
 
          ### Criação da label e entrada da cidade
-        self.lb_cidade = Label(self.frame_1, text="Cidade")
+        self.lb_cidade = Label(self.frame_1, text="Cidade", bg='#dfe3ee', fg='#107db2')
         self.lb_cidade.place(relx=0.5, rely=0.6)
 
         self.cidade_entry = Entry(self.frame_1)
